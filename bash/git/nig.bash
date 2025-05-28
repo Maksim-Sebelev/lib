de2fb89acc804a0e8395f9b556b151fde0d16ee9
Maksim-Sebelev <7905111@list.ru>
2025-05-29 01:55:09 +0300

    ZoV

--- bash/git/nig.bash
+++ bash/git/nig.bash
@@ -7,23 +7,37 @@ red_concole_color="\x1b[31m"
 console_color_reset="\x1b[0m"
 
 if [ "$#" -eq 0 ]; then
-    echo -e "$red_concole_color no arguments."
+    echo -e $red_concole_color"no arguments."
     echo -e "expected files and/or directories.$console_color_reset"
     exit 0
 fi
 
 for arg in "$@"; do
-    # arg="${arg%/}" # remove '/' from end of directory name
 
     if [ -f $arg ]; then
-        echo "removing file: '$arg'"
-        git rm --cached "$arg"
+
+        if git ls-files --error-unmatch --quiet -- "$arg" >/dev/null 2>&1; then
+            echo "removing file: '$arg'"
+            git rm --cached "$arg"
+
+        else
+            echo "'$arg' already removed from repository or doesn't marked in .gitignore."
+
+        fi
 
     elif [ -d "$arg" ]; then
-        echo "removing directory: '$arg'"
-        git rm --cached -r "$arg"
+
+        if git ls-files --error-unmatch --quiet -- "$arg" >/dev/null 2>&1; then
+            echo "removing file: '$arg'"
+            git rm --cached -r "$arg"
+
+        else
+            echo "'$arg' already removed from repository or doesn't marked in '.gitignore'."
+
+        fi
 
     else
         echo -e "$red_concole_color'$arg' doesn't exist.$console_color_reset"
     fi
+
 done
