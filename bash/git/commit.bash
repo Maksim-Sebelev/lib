8c4a819694d57234a1271573fcec4565b75652e2
Maksim-Sebelev <7905111@list.ru>
2025-05-29 02:05:25 +0300

    "fuckyou"

--- bash/git/commit.bash
+++ bash/git/commit.bash
@@ -3,12 +3,14 @@
 if [ "$#" -eq 0 ]; then
     commit="ZoV"
 elif [ "$#" -eq 1 ]; then
-    commit=$1
+    commit=\"$1\"
 else
     echo "Too many arguments."
     echo "Expected only commit name."
     exit 0
 fi
+
+echo "commmit = '$commit'"
 
 git pull origin main
 git add --all
