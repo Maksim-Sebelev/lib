7688cc91baa212f18d6cb93201e77c1e4b624b6d
Maksim-Sebelev <7905111@list.ru>
2025-06-03 08:59:22 +0300

    fixed copy.bash

--- bash/git/copy.bash
+++ bash/git/copy.bash
@@ -3,25 +3,29 @@ if [ "$#" -eq 0 ]; then
     exit 0;
 fi
 
-copy_dir=../copys/
-
+copy_dir=../.copys
 mkdir -p $copy_dir
 
 if [ "$1" == "clean" ]; then
-    read -p "Are you ready, that you want to remove all dir for copys? [y/n]: " user_input
+    read -p "Are you sure, that you want to REMOVE all dir for copys? [y/n]: " user_input
     if [ "$user_input" == "y" ] || [ -z "$user_input" ]; then
-        rm -rf $copy_dir/*
+        rm -rf $copy_dir/{*,.[!.]*,..?*}
     fi
     exit 0;
 fi
 
-
 for arg in "$@"; do
     if [ -f "$arg" ]; then
-        cp $arg $copy_dir
+        cp $arg $copy_dir/
     elif [ -d "$arg" ]; then
-        cp -r $arg $arg $copy_dir
+        if [ "$arg" == "." ]; then
+            now_dir=$(basename "$(pwd)")
+            mkdir -p $copy_dir/$now_dir
+            cp -r $arg $copy_dir/$now_dir/
+        else
+            cp -r $arg $copy_dir/
+        fi
     else
-        echo "$arg - doesn't exist"
+        echo "$arg - doesn't exists"
     fi
 done
