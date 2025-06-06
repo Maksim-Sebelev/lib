c1f2b0d40f1a720bd02e7319fbcd9222d83d3a82
Maksim-Sebelev <7905111@list.ru>
2025-06-06 09:16:29 +0300

    fuck

--- bash/assets/struct.bash
+++ bash/assets/struct.bash
@@ -1,8 +1,22 @@
 #!/bin/bash/
 
+mkdir -p project_struct/
+
 if [ "$#" -eq 0 ]; then
-    mkdir -p project_struct/
     tree --gitignore > project_struct/project_struct.txt 
-else
-    echo "No arguments expected"
+    tree > project_struct/all_project_struct.txt
+
+elif [ "$#" -eq 1 ]; then
+
+    if [ "$1" == "-c" ] || [ "$1" == "--console" ]; then
+        tree -C --gitignore
+
+    elif [ "$1" == "-a" ] || [ "$1" == "--all" ]; then
+        tree -C
+
+    else
+        echo "undef arg"
+        exit 1
+    fi
+
 fi
